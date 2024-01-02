b0e25ac5b0261cc73247f41136811aaf4387d3cd
Cassiano Castro <development.castro@gmail.com>
2024-01-02 10:44:28 -0300

    Formatting adjust, removed unnecessary whitespaces

--- src/Fatoriais.c
+++ src/Fatoriais.c
@@ -1,41 +1,49 @@
+#include <iso646.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <iso646.h>
 
-int recursive( int number )
+int recursive(int number)
 {
-    if (( number == 1 ) or ( number == 0 ))
+    if ( ( number == 1 ) or ( number == 0 ) )
         return 1;
-    return number * recursive( number - 1 );
+
+    return number * recursive(number - 1);
 }
 
-int iterative( int number )
+int iterative(int number)
 {
     int fatorial = 1;
-    for ( ; number > 1; number--)
+
+    for ( ; number > 1; number-- )
 	{
         fatorial *= number;
     }
+
     return fatorial;
 }
 
-int main(int argc, char const *argv[])
+/**
+ *
+ */
+int main(int argc, const char* argv[])
 {
     unsigned int number = 0, option = 0;
 
     printf("Insira um número para o qual deseja calcular seu fatorial: ");
-    scanf ( "%i", &number );
+    scanf("%i", &number);
+
     printf(
         "Selecione a forma de calcular:\n"
         "1. Iterativa | 2. Recursiva\n"
-        "Opção? "
+        "Opção\? "
     );
-    scanf ( "%i", &option );
+    scanf("%i", &option);
 
     printf(
         "\nFatorial calculado: %d\n",
-        ( option == 1 ) ? iterative( number ) : recursive( number )
+        ( option == 1 ) ? iterative(number) : recursive(number)
     );
-    return ( EXIT_SUCCESS );
+
+    return EXIT_SUCCESS;
 }
