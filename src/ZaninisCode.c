a6499c772a0973c9cf573625d77aebb17070b30e
Cassiano Castro <development.castro@gmail.com>
2024-01-03 15:21:55 -0300

    Formatting adjust

--- src/ZaninisCode.c
+++ src/ZaninisCode.c
@@ -8,24 +8,34 @@ void cripto(char* texto)
 {
 	short int tamanho = strlen(texto), i;
 
-	for (i = 0; i < tamanho; i++)
+	for ( i = 0; i < tamanho; i++ )
     {
-		if (texto[i] >= 97 and texto[i] <= 116) {
-		    if (texto[i] > 122) texto[i] -= 26;
+		if ( texto[i] >= 97 and texto[i] <= 116 )
+        {
+		    if ( texto[i] > 122 )
+            {
+                texto[i] -= 26;
+            }
+
 	    	texto[i] += 6;
 	   	}
 	}
 }
 
-void decripto(char *texto)
+void decripto(char* texto)
 {
   	short int tamanho = strlen(texto), i;
 
-	for (i = 0; i < tamanho; i++)
+	for ( i = 0; i < tamanho; i++ )
     {
-	    if (texto[i] >= 97 and texto[i] <= 122) {
-	       texto[i] -= 6;
-	       if (texto[i] < 97) texto[i] += 26;
+	    if ( texto[i] >= 97 and texto[i] <= 122 )
+        {
+            texto[i] -= 6;
+
+            if ( texto[i] < 97 )
+            {
+                texto[i] += 26;
+            }
 		}
 	}
 }
@@ -36,23 +46,27 @@ int main(int argc, char const *argv[])
 
     char opcao = '0', frase[50];
 
-    while (opcao not_eq '3')
+    while ( opcao not_eq '3' )
     {
         printf("1 - Criptografar\n2 - Descriptografar\n3 - Sair\nOpção\? ");
         scanf(" %c", &opcao);
 
-        switch (opcao)
+        switch ( opcao )
         {
             case '1':
                 printf("Digite a frase: ");
                 gets(frase);
+
                 cripto(frase);
+
                 printf("Texto criptografado: %s\n", frase);
                 break;
             case '2':
                 printf("Digite a frase: ");
                 gets(frase);
+
                 decripto(frase);
+
                 printf("Texto criptografado: %s\n", frase);
                 break;
             case '3':
@@ -62,5 +76,6 @@ int main(int argc, char const *argv[])
                 puts("Opcao Inválida!");
         }
     }
-    return (EXIT_SUCCESS);
+
+    return EXIT_SUCCESS;
 }
