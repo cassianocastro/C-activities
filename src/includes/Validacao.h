a39f7b555c9f2229040ad788989150cd6b4d1eb6
Cassiano Castro <development.castro@gmail.com>
2024-01-03 15:56:47 -0300

    Formatting adjust

--- src/includes/Validacao.h
+++ src/includes/Validacao.h
@@ -21,7 +21,7 @@ enum Meses
 
 bool validadata(uint8_t dia, uint8_t mes, uint16_t ano)
 {
-    switch (mes)
+    switch ( mes )
     {
         case JANEIRO:
         case MARCO:
@@ -32,13 +32,18 @@ bool validadata(uint8_t dia, uint8_t mes, uint16_t ano)
         case DEZEMBRO:
             return dia <= 31 and dia > 0;
         case FEVEREIRO:
-            if(((ano % 4 == 0) and (ano % 100 not_eq 0)) or (ano % 400 == 0))
+            if( ((ano % 4 == 0) and (ano % 100 not_eq 0)) or (ano % 400 == 0) )
             {
                 return dia <= 29 and dia > 0;
-            } else if (dia <= 28 and dia > 0)
+            }
+            else if ( dia <= 28 and dia > 0 )
+            {
                 return true;
+            }
             else
+            {
                 return false;
+            }
         case ABRIL:
         case JUNHO:
         case SETEMBRO:
