95fe819b87fe1230ac1a7e41a5442f00466283f8
Cassiano Castro <development.castro@gmail.com>
2024-01-03 15:57:39 -0300

    Reordered includes

--- src/includes/PIP.h
+++ src/includes/PIP.h
@@ -1,9 +1,9 @@
+#include <ctype.h>
+#include <iso646.h>
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <locale.h>
-#include <iso646.h>
-#include <ctype.h>
 
 typedef unsigned char byte;
 
