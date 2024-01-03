bd35668e66f1d654aecb44d05b111b2e495b234d
Cassiano Castro <development.castro@gmail.com>
2024-01-03 14:51:34 -0300

    Reordered includes

--- src/Exercicio_PCs.c
+++ src/Exercicio_PCs.c
@@ -1,10 +1,10 @@
+#include <ctype.h>
+#include <iso646.h>
+#include <locale.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <iso646.h>
-#include <stdbool.h>
-#include <locale.h>
-#include <ctype.h>
 
 #define TAM_STRING 30
 
