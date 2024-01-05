cf9601ba4f7259b8e54c67e582cf0eb91362f517
Cassiano Castro <development.castro@gmail.com>
2024-01-05 16:22:44 -0300

    Added main body and call to start() function

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <locale.h>
+#include "./src/includes/Evaluation.h"
+
+/**
+ *
+ */
+int main(int argc, const char* argv[])
+{
+	setlocale(LC_ALL, "");
+
+    start();
+
+	return EXIT_SUCCESS;
+}
