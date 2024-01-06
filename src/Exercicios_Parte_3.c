05b3f6ee62d866247bb0320f3d2ace087b1d088b
Cassiano Castro <development.castro@gmail.com>
2024-01-06 16:52:08 -0300

    Removed unnecessary whitespaces

--- src/Exercicios_Parte_3.c
+++ src/Exercicios_Parte_3.c
@@ -8,19 +8,19 @@ typedef unsigned short ushort;
 
 const ushort TAMANHO_MATRIZ = 6, LIMITE_RANDOMICO = 100;
 
-void testandoAlinhamento( void )
+void testandoAlinhamento(void)
 {
 	int valor = 1;
 
-    printf("Justificado a direita  => %5d\n",valor);
-    printf("Justificado a esquerda => %-5d\n",valor);
+    printf("Justificado a direita  => %5d\n", valor);
+    printf("Justificado a esquerda => %-5d\n", valor);
 
 	uint_fast8_t fast = 190;
 
-	printf("Poder: %hhu\nSizeof: %hhu", fast, sizeof( uint_fast8_t ));
+	printf("Poder: %hhu\nSizeof: %hhu", fast, sizeof(uint_fast8_t));
 }
 
-void gerarMatriz( void )
+void gerarMatriz(void)
 {
 	ushort lin = 0, col = 0;
 	ushort m[TAMANHO_MATRIZ][TAMANHO_MATRIZ];
@@ -40,30 +40,30 @@ void gerarMatriz( void )
 	}
 }
 
-char* verificarOpcao( char* opcao )
+char* verificarOpcao(char* opcao)
 {
 	while ( strcmp(opcao, "sim") != 0 and strcmp(opcao, "nao") != 0 )
     {
 		printf("Resposta inv�lida. Digite novamente: ");
-		gets( opcao );
+		gets(opcao);
 	}
 
 	return opcao;
 }
 
-int main( void )
+int main(void)
 {
-	setlocale ( LC_ALL, "" );
-	srand ( time(NULL) );
+	setlocale(LC_ALL, "");
+	srand(time(NULL));
 
     gerarMatriz();
 
 	char opcao[3] = "\0\0\0";
 
-	printf ("\nDeseja realizar outra opera��o [sim/nao]? ");
-	gets   ( opcao );
+	printf("\nDeseja realizar outra opera��o [sim/nao]? ");
+	gets(opcao);
 
-	printf ("\nOp��o escolhida: %s", verificarOpcao( opcao ));
+	printf("\nOp��o escolhida: %s", verificarOpcao(opcao));
 
 	return 0;
 }
