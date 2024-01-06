9fe5ed93f8330641b22ed1aca1ad92ffef76908a
Cassiano Castro <development.castro@gmail.com>
2024-01-06 16:12:51 -0300

    Reordered includes

--- src/Exercicios_Parte_1.c
+++ src/Exercicios_Parte_1.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include <inttypes.h>
 #include <iso646.h>
 #include <locale.h>
-#include <inttypes.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef unsigned char Byte;
 
