1683baa169563e581554335bfad9ca9a4978a665
Cassiano Castro <development.castro@gmail.com>
2024-01-08 17:26:56 -0300

    Added typedefs statements

--- src/includes/Computer.h
+++ src/includes/Computer.h
@@ -2,6 +2,54 @@
 
 #define COMPUTER_H
 
+#include <ctype.h>
+#include <iso646.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define TAM_STRING 30
+
+typedef struct
+{
+    char storageType;
+    char model[TAM_STRING];
+    char mark[TAM_STRING];
+    char processor[TAM_STRING];
+
+    int memory;
+    int storage;
+}
+Computer;
+
+typedef enum
+{
+	EXIT,
+    CREATE,
+    READ,
+    UPDATE,
+    DELETE,
+    SEARCH
+}
+Opcoes;
+
+typedef enum
+{
+	MARK,
+    STORAGE_TYPE,
+    STORAGE,
+    MEMORY,
+    MODEL,
+    PROCESSOR
+}
+Dados;
+
+typedef unsigned char byte;
+
+Computer* inventary;
+
+unsigned short numero;
 
 #endif
