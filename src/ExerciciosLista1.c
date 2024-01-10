65d8f5dcc2a6c1afcd79a097cf4004630d323bda
Cassiano Castro <development.castro@gmail.com>
2024-01-10 17:02:55 -0300

    Comments before function definitions has maded docblocks

--- src/ExerciciosLista1.c
+++ src/ExerciciosLista1.c
@@ -23,10 +23,10 @@ typedef enum
     EXERCICIO10
 } Opcoes;
 
-/*
-    Faça um programa que converta a moeda real em dólar, informando-se apenas a
-    cotação do dólar e o valor em reais.
-*/
+/**
+ * Faça um programa que converta a moeda real em dólar,
+ * informando-se apenas a cotação do dólar e o valor em reais.
+ */
 void exercicio1(void)
 {
     float cotacao_dolar = 0.0f, valor_reais = 0.0f;
@@ -40,12 +40,14 @@ void exercicio1(void)
     printf("O valor em dólares é: U$ %.2f", ( valor_reais / cotacao_dolar ));
 }
 
-/*
-    Faça um programa em que ao digitar um valor de temperatura em graus Celcius o
-    sistema converta em Fahrenheit e Kelvin, sabendo se que para conversão Celcius X
-    Fahrenheit a fórmula aplicada será Fahrenheit = Celcius × 1,8 + 32 e para conversão
-    Celcius X Kelvin a fórmula aplicada será Kelvin = Celcius + 273,15
-*/
+/**
+ * Faça um programa em que ao digitar um valor de temperatura em graus
+ * Celcius o sistema converta em Fahrenheit e Kelvin,
+ * sabendo se que para conversão Celcius X Fahrenheit a fórmula
+ * aplicada será Fahrenheit = Celcius × 1,8 + 32 e para
+ * conversão Celcius X Kelvin a fórmula aplicada
+ * será Kelvin = Celcius + 273,15
+ */
 void exercicio2(void)
 {
     float celsius = 0.0f;
@@ -62,13 +64,15 @@ void exercicio2(void)
 	);
 }
 
-/*
-    Faça um programa que leia o sexo da pessoa e o valor de uma compra. Se a
-    pessoa for do sexo masculino, indicado pela letra 'm', então ela recebe um desconto de
-    10% sobre o valor da compra, se for do sexo feminino, indicado pela letra 'f', então é
-    concedido um desconto de 15% sobre o valor da compra. O programa deverá exibir o valor
-    bruto, o desconto concedido e o valor líquido.
-*/
+/**
+ * Faça um programa que leia o sexo da pessoa e o valor de uma compra.
+ * Se a pessoa for do sexo masculino, indicado pela letra 'm',
+ * então ela recebe um desconto de 10% sobre o valor da compra,
+ * se for do sexo feminino, indicado pela letra 'f',
+ * então é concedido um desconto de 15% sobre o valor da compra.
+ * O programa deverá exibir o valor bruto, o desconto concedido e o
+ * valor líquido.
+ */
 void exercicio3(void)
 {
     float valor_compra = 0.0f, desconto = 0.0f, valor_liquido = 0.0f;
@@ -105,12 +109,14 @@ void exercicio3(void)
 	);
 }
 
-/*
-    Faça um programa que leia um valor. Se o valor for menor que R$ 100,00, então é
-    concedido um desconto de 5%, se o valor for superior a R$ 100,00, então há um acréscimo
-    de 7% sobre o valor. O programa deverá exibir o valor bruto, o desconto concedido e o
-    valor líquido.
-*/
+/**
+ * Faça um programa que leia um valor. Se o valor for menor que R$ 100,00,
+ * então é concedido um desconto de 5%,
+ * se o valor for superior a R$ 100,00,
+ * então há um acréscimo de 7% sobre o valor.
+ * O programa deverá exibir o valor bruto, o desconto concedido e o
+ * valor líquido.
+ */
 void exercicio4(void)
 {
     float valor = 0.0f, valor_liquido = 0.0f;
@@ -148,11 +154,12 @@ void exercicio4(void)
     }
 }
 
-/*
-    Faça um programa que leia a série que o aluno estuda. Se o aluno estuda entre a 1
-    e a 4 série, então o programa deverá escrever “Turno da manhã”, se o aluno estuda da 5 a
-    8 série, então o programa deverá escrever “Turno da tarde”.
-*/
+/**
+ * Faça um programa que leia a série que o aluno estuda.
+ * Se o aluno estuda entre a 1 e a 4 série, então o programa deverá
+ * escrever “Turno da manhã”, se o aluno estuda da 5 a 8 série,
+ * então o programa deverá escrever “Turno da tarde”.
+ */
 void exercicio5(void)
 {
     Byte serie_informada = 0;
@@ -174,12 +181,12 @@ void exercicio5(void)
     }
 }
 
-/*
-    Faça um programa que leia 3 valores. Se a soma dos 3 valores for superior a R$
-    1000,00, então o programa desconta 10% da soma dos 3 valores , senão o desconto será
-    de 8%. O programa deverá exibir o valor bruto(soma dos 3 valores), o desconto concedido
-    e o valor líquido.
-*/
+/**
+ * Faça um programa que leia 3 valores. Se a soma dos 3 valores for
+ * superior a R$ 1000,00, então o programa desconta 10% da soma dos 3 valores,
+ * senão o desconto será de 8%. O programa deverá exibir o valor
+ * bruto(soma dos 3 valores), o desconto concedido e o valor líquido.
+ */
 void exercicio6(void)
 {
     float primeiro_valor = 0.0f, segundo_valor  = 0.0f, terceiro_valor = 0.0f;
@@ -210,15 +217,16 @@ void exercicio6(void)
 	);
 }
 
-/*
-    Faça um programa que leia 4 números. Se a soma dos dois primeiros números for
-    maior que a soma dos 2 últimos números, então será concedido um desconto sobre o total
-    bruto(soma dos 4 valores) de 20%, se a soma dos dois últimos números for maior que a
-    soma dos 2 primeiros números, então há uma acréscimo sobre o total bruto(soma dos 4
-    valores) de 20%.O programa deverá exibir a soma dos 2 primeiros números, a soma dos 2
-    últimos números, o valor bruto(soma dos 4 valores), o desconto ou acréscimo concedidos e
-    o valor líquido.
-*/
+/**
+ * Faça um programa que leia 4 números. Se a soma dos dois primeiros números
+ * for maior que a soma dos 2 últimos números, então será concedido um
+ * desconto sobre o total bruto(soma dos 4 valores) de 20%, se a soma dos
+ * dois últimos números for maior que a soma dos 2 primeiros números,
+ * então há uma acréscimo sobre o total bruto(soma dos 4 valores) de 20%.
+ * O programa deverá exibir a soma dos 2 primeiros números, a soma dos 2
+ * últimos números, o valor bruto(soma dos 4 valores), o desconto ou acréscimo
+ * concedidos e o valor líquido.
+ */
 void exercicio7(void)
 {
     float valor1 = 0.0f, valor2 = 0.0f, valor3 = 0.0f, valor4 = 0.0f, valor_liquido = 0.0f;
@@ -269,10 +277,10 @@ void exercicio7(void)
     }
 }
 
-/*
-    Faça um programa que leia 5 números, em seguida, demonstre qual o maior
-    número digitado.
-*/
+/**
+ * Faça um programa que leia 5 números, em seguida,
+ * demonstre qual o maior número digitado.
+ */
 void exercicio8(void)
 {
     Byte vetor[5] = {0, 0, 0, 0, 0}, maior_numero = 0;
@@ -291,10 +299,10 @@ void exercicio8(void)
     printf("O maior número informado foi: %hhu", maior_numero);
 }
 
-/*
-    Faça um programa que leia 5 números, em seguida, demonstre qual o maior e o
-    menor número digitado.
-*/
+/**
+ * Faça um programa que leia 5 números, em seguida,
+ * demonstre qual o maior e o menor número digitado.
+ */
 void exercicio9(void)
 {
     Byte vetor[5] = { 0, 0, 0, 0, 0 };
@@ -324,10 +332,10 @@ void exercicio9(void)
 	);
 }
 
-/*
-    Faça um programa que leia 10 números, em seguida, mostre quantos dos
-    números digitados é par e quantos números digitados é ímpar.
-*/
+/**
+ * Faça um programa que leia 10 números, em seguida, mostre quantos dos
+ * números digitados é par e quantos números digitados é ímpar.
+ */
 void exercicio10(void)
 {
     Byte vetor[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
