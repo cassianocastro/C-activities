fe1b2e6621ece2edce53d73befc06dfd408ab99b
Cassiano Castro <development.castro@gmail.com>
2024-01-10 17:16:11 -0300

    Add type definitions and necessary header files

--- src/includes/ExerciciosLista1.h
+++ src/includes/ExerciciosLista1.h
@@ -2,6 +2,32 @@
 
 #define EXERCICIOS_LISTA_1_H
 
+#include <ctype.h>
+#include <iso646.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+typedef unsigned char Byte;
+
+/**
+ *
+ */
+typedef enum
+{
+    SAIR,
+    EXERCICIO1,
+    EXERCICIO2,
+    EXERCICIO3,
+    EXERCICIO4,
+    EXERCICIO5,
+    EXERCICIO6,
+    EXERCICIO7,
+    EXERCICIO8,
+    EXERCICIO9,
+    EXERCICIO10
+} Opcoes;
 
 #endif
