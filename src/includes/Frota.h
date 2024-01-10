b0591bb4e989576b148b359a454ac420e3b957f6
Cassiano Castro <development.castro@gmail.com>
2024-01-10 16:16:23 -0300

    Added type definitions and necessary headers

--- src/includes/Frota.h
+++ src/includes/Frota.h
@@ -2,6 +2,39 @@
 
 #define FROTA_H
 
+#include <iso646.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define STRING_SIZE 20
+
+typedef unsigned char byte;
+
+typedef enum
+{
+    EXIT,
+    CREATE,
+    READ,
+    DELETE
+}
+Options;
+
+typedef struct
+{
+	char model[STRING_SIZE];
+	char mark[STRING_SIZE];
+	char placa[STRING_SIZE];
+	char color[STRING_SIZE];
+
+	unsigned short release;
+}
+Car;
+
+Car frota[10];
+
+const unsigned int ARRAY_SIZE = 10u;
 
 #endif
