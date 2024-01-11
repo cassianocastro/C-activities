dd82104939809da2cb2b65f4d32051cc46f12fb5
Cassiano Castro <development.castro@gmail.com>
2024-01-11 16:31:24 -0300

    Added type definitions

--- src/includes/Library.h
+++ src/includes/Library.h
@@ -2,6 +2,39 @@
 
 #define LIBRARY_H
 
+#include <ctype.h>
+#include <iso646.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define TAMANHO 50
+
+typedef unsigned char byte;
+
+typedef enum
+{
+    EXIT,
+    CREATE,
+    READ,
+    DELETE,
+    SEARCH
+}
+Options;
+
+typedef struct
+{
+	char title[TAMANHO];
+	char author[TAMANHO];
+	char publishing[TAMANHO];
+	char subject[TAMANHO];
+
+	unsigned short release;
+}
+Book;
+
+Book library[10];
 
 #endif
