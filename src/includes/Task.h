bace99217e1d993194b6fa1c1bcd7f1f84b25975
Cassiano Castro <development.castro@gmail.com>
2024-01-12 15:40:59 -0300

    Added type definitions and function prototypes

--- src/includes/Task.h
+++ src/includes/Task.h
@@ -2,6 +2,54 @@
 
 #define TASK_H
 
+#include <iso646.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+/**
+ *
+ */
+typedef enum
+{
+    EXIT,
+	ASCENDING,
+    DESCENDING
+}
+Options;
+
+extern const unsigned char VECTOR_SIZE, RANDOM_LIMIT;
+
+/**
+ *
+ */
+void printVector(int*);
+
+/**
+ *
+ */
+void ascendingOrder(int*);
+
+/**
+ *
+ */
+void descendingOrder(int*);
+
+/**
+ *
+ */
+void fillVectorWithRandomNumbers(int*);
+
+/**
+ *
+ */
+const unsigned int getMainMenuChoice(void);
+
+/**
+ *
+ */
+void start(void);
 
 #endif
