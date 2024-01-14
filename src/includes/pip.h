a2e7ed095bf9e6e2216ac7e9e1052e048e11792a
Cassiano Castro <development.castro@gmail.com>
2024-01-14 16:45:49 -0300

    Removed body of functions

--- src/includes/pip.h
+++ src/includes/pip.h
@@ -24,112 +24,32 @@ enum Meses
     DEZEMBRO
 };
 
-int geraPar(int limite)
-{
-    int n = rand() % limite;
+/**
+ *
+ */
+int geraPar(int);
 
-    if ( n % 2 == 0 )
-    {
-        return n;
-    }
+/**
+ *
+ */
+int geraImpar(int);
 
-    return (n + 1);
-}
+/**
+ *
+ */
+int geraprimo(int);
 
-int geraImpar(int limite)
-{
-    int n = rand() % limite;
+/**
+ *
+ */
+char verifyResponse(char);
 
-    if ( n % 2 != 0 )
-    {
-        return n;
-    }
+/**
+ *
+ */
+bool isValid(int, int, int);
 
-    return (n - 1);
-}
-
-int geraprimo(int limite)
-{
-    int num, x, cd;
-
-    do {
-        cd  = 0;
-        num = rand() % limite;
-
-        for ( x = 1; x <= num; x++ )
-        {
-            if ( num % x == 0 ) cd++;
-
-            if ( cd > 2 ) break;
-        }
-    } while ( cd != 2 );
-
-    return num;
-}
-
-char verifyResponse(char response)
-{
-    response = tolower(response);
-
-	while ( response != 's' and response != 'n' )
-    {
-		printf("\nOpção inválida. Digite novamente: ");
-		scanf(" %c", &response);
-	}
-
-	return response;
-}
-
-bool isValid(int dia, int mes, int ano)
-{
-	switch ( mes )
-    {
-		case FEVEREIRO:
-			if ( ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0 )
-            {
-				return dia > 0 and dia <= 29;
-            }
-			else if ( dia <= 28 and dia > 0 )
-            {
-                return true;
-            }
-
-			return false;
-		case JANEIRO:
-		case MARCO:
-		case MAIO:
-		case JULHO:
-		case AGOSTO:
-		case OUTUBRO:
-		case DEZEMBRO:
-			return dia >= 1 and dia <= 31;
-		case ABRIL:
-		case JUNHO:
-		case SETEMBRO:
-		case NOVEMBRO:
-			return dia >= 1 and dia <= 30;
-		default:
-			return false;
-	}
-}
-
-char* nameOfThis(byte month)
-{
-	switch ( month )
-    {
-		case JANEIRO:    return "Janeiro..";
-		case FEVEREIRO:  return "Fevereiro";
-		case MARCO:      return "Março....";
-		case ABRIL:      return "Abril....";
-		case MAIO:       return "Maio.....";
-		case JUNHO:      return "Junho....";
-		case JULHO:      return "Julho....";
-		case AGOSTO:	 return "Agosto...";
-		case SETEMBRO:	 return "Setembro.";
-		case OUTUBRO:	 return "Outubro..";
-		case NOVEMBRO:   return "Novembro.";
-		case DEZEMBRO:   return "Dezembro.";
-	}
-
-	return NULL;
-}
+/**
+ *
+ */
+char* nameOfThis(byte);
