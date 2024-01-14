72f4f0458482abefa6670b50542114a079d8dc26
Cassiano Castro <development.castro@gmail.com>
2024-01-14 16:16:40 -0300

    Added type definitions and std headers inclusion

--- src/includes/Criptographer.h
+++ src/includes/Criptographer.h
@@ -2,6 +2,54 @@
 
 #define CRIPTOGRAPHER_H
 
+#ifdef CRIPTOGRAPHER_V1
 
+#include <iso646.h>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAMANHO_FRASE 100
+
+typedef unsigned char byte;
+
+typedef enum
+{
+    SAIR,
+    CRIPTOGRAFAR,
+    DESCRIPTOGRAFAR
+}
+Options;
+
+#elif CRIPTOGRAPHER_V2
+
+#include <iso646.h>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAMANHO_FRASE 100
+
+typedef unsigned int byte;
+
+typedef enum
+{
+    SAIR,
+    CRIPTOGRAFAR,
+    DESCRIPTOGRAFAR
+}
+Options;
+
+#elif CRIPTOGRAPHER_ZANINI
+
+#include <iso646.h>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif
 
 #endif
