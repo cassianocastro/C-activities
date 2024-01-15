eb87d03cc8a0951664c65b85bf9e8ac78e0552f5
Cassiano Castro <development.castro@gmail.com>
2024-01-15 16:17:12 -0300

    Removed functions out of context

--- src/Exercicios_Parte_2.c
+++ src/Exercicios_Parte_2.c
@@ -140,37 +140,6 @@ void maiornovetor(ushort* vector)
 	printf("\nO maior valor presente no vetor é: %.2hu", maior);
 }
 
-void converterMoeda(void)
-{
-	float real = 0.0f, dolar = 0.0f;
-
-	printf("\nInforme o valor em R$: ");
-	scanf("%f", &real);
-
-	printf("O valor atual do dólar: ");
-	scanf("%f", &dolar);
-
-	printf("\nA quantia correspondente em dólares é de: U$ %.2f", real / dolar);
-}
-
-void converterTemperatura(void)
-{
-	float celsius = 0.0f;
-
-	printf("\nInforme a temperatura em graus Celsius: ");
-	scanf("%f", &celsius);
-
-    unsigned char opcao = 0;
-
-	printf("\nDigite 1 para conversão em Farenheit ou 0 para Kelvin. Opção\? ");
-	scanf("%hhu", &opcao);
-
-	printf(
-        "A temperatura em graus Kelvin é: %.1f\n",
-        ( opcao not_eq 1 ) ? (celsius + 273.15) : ((celsius * 9 / 5) + 32)
-    );
-}
-
 /**
  *
  */
