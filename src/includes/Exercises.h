352d970adc7d7dfdff17ef9bbdb16aaa74117f39
Cassiano Castro <development.castro@gmail.com>
2024-01-16 15:02:04 -0300

    Inclusion of standard header files and typedef statements

--- src/includes/Exercises.h
+++ src/includes/Exercises.h
@@ -4,9 +4,47 @@
 
 #ifdef EX_P1
 
+#include <inttypes.h>
+#include <iso646.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef unsigned char byte;
+
 #elif EX_P2
 
+#include <iso646.h>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+typedef unsigned short int ushort;
+
+typedef enum
+{
+	REVERSE = 1,
+    ORDER,
+    MAX_VAL
+}
+Options;
+
+extern const ushort VECTOR_SIZE, RANDOM_LIMIT;
+
 #elif EX_P3
+
+#include <iso646.h>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+typedef unsigned short ushort;
 
 #endif
 
