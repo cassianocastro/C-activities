ee4ae13221134febb81b8d9f920b0e164b476f27
Cassiano Castro <development.castro@gmail.com>
2024-01-17 16:38:29 -0300

    Updated enum memebers name to match with has declared in the header file

--- src/app/Validation.c
+++ src/app/Validation.c
@@ -4,15 +4,15 @@ bool validadata(uint8_t dia, uint8_t mes, uint16_t ano)
 {
     switch ( mes )
     {
-        case JANEIRO:
-        case MARCO:
-        case MAIO:
-        case JULHO:
-        case AGOSTO:
-        case OUTUBRO:
-        case DEZEMBRO:
+        case JANUARY:
+        case MARCH:
+        case MAY:
+        case JULY:
+        case AUGUST:
+        case OCTOBER:
+        case DECEMBER:
             return dia <= 31 and dia > 0;
-        case FEVEREIRO:
+        case FEBRUARY:
             if( ((ano % 4 == 0) and (ano % 100 not_eq 0)) or (ano % 400 == 0) )
             {
                 return dia <= 29 and dia > 0;
@@ -25,10 +25,10 @@ bool validadata(uint8_t dia, uint8_t mes, uint16_t ano)
             {
                 return false;
             }
-        case ABRIL:
-        case JUNHO:
-        case SETEMBRO:
-        case NOVEMBRO:
+        case APRIL:
+        case JUNE:
+        case SEPTEMBER:
+        case NOVEMBER:
             return dia <= 30 and dia > 0;
         default:
             return false;
