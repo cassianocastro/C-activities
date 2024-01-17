04b9aa472834ee0a61b64519518197c78bc46c5c
Cassiano Castro <development.castro@gmail.com>
2024-01-17 17:16:51 -0300

    Added preprocessor directives to verify which code should be compiled

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <locale.h>
+#include <stdlib.h>
+
+#ifdef EVALUATION
+
 #include "./src/includes/Evaluation.h"
+
+#elif defined AGENDA
+
+#include "./src/includes/Agenda.h"
+
+#endif
 
 /**
  *
