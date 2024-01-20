4a36bbfcbf016c2deab8b39a707949e9470cddef
Cassiano Castro <development.castro@gmail.com>
2024-01-20 17:14:00 -0300

    Removed old content

--- src/includes/model/AgendaModel.h
+++ src/includes/model/AgendaModel.h
@@ -1,35 +1,7 @@
-#ifndef VALIDATION_H
+#ifndef AGENDA_MODEL_H
 
-#define VALIDATION_H
+#define AGENDA_MODEL_H
 
-#include <inttypes.h>
-#include <iso646.h>
-#include <stdbool.h>
-#include <stdio.h>
 
-/**
- *
- */
-typedef enum
-{
-	JANUARY = 1,
-    FEBRUARY,
-    MARCH,
-    APRIL,
-    MAY,
-    JUNE,
-    JULY,
-    AUGUST,
-    SEPTEMBER,
-    OCTOBER,
-    NOVEMBER,
-    DECEMBER
-}
-Months;
-
-/**
- *
- */
-bool validadata(uint8_t, uint8_t, uint16_t);
 
 #endif
