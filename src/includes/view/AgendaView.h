2638b7e0eb08d32efaa7f601e522f0d7dd05d4a3
Cassiano Castro <development.castro@gmail.com>
2024-01-20 18:07:33 -0300

    Added function prototypes

--- src/includes/view/AgendaView.h
+++ src/includes/view/AgendaView.h
@@ -2,6 +2,31 @@
 
 #define AGENDA_VIEW_H
 
+#include "../model/AgendaModel.h"
 
+/**
+ *
+ */
+const unsigned int showMainMenu(void);
+
+/**
+ *
+ */
+const unsigned int showUpdateMenu(void);
+
+/**
+ *
+ */
+void showContactForm(byte);
+
+/**
+ *
+ */
+void printContact(const Contact* const);
+
+/**
+ *
+ */
+byte getContactID(void);
 
 #endif
