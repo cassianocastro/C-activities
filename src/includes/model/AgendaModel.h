395a6258b8e2832570a8a47dbb363d81d268520e
Cassiano Castro <development.castro@gmail.com>
2024-01-20 18:07:14 -0300

    Added type definitions

--- src/includes/model/AgendaModel.h
+++ src/includes/model/AgendaModel.h
@@ -2,6 +2,34 @@
 
 #define AGENDA_MODEL_H
 
+typedef unsigned char byte;
 
+/**
+ *
+ */
+typedef struct
+{
+	byte day;
+    byte month;
+
+    unsigned short int year;
+}
+BirthDate;
+
+/**
+ *
+ */
+typedef struct
+{
+	BirthDate dn;
+
+    byte age;
+
+    char name[51];
+    char city[21];
+    char phone[17];
+    char sex;
+}
+Contact;
 
 #endif
