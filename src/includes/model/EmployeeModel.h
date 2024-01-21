9c9084d57754a50376211db4cf2a640a446d8e99
Cassiano Castro <development.castro@gmail.com>
2024-01-21 01:01:46 -0300

    Added type definitions and function prototype

--- src/includes/model/EmployeeModel.h
+++ src/includes/model/EmployeeModel.h
@@ -2,6 +2,42 @@
 
 #define EMPLOYEES_MODEL_H
 
+#define SIZE 50
+#define MONTHS 12
 
+typedef unsigned char byte;
+
+/**
+ *
+ */
+typedef struct
+{
+	int day;
+    int month;
+    int year;
+}
+BirthDate;
+
+/**
+ *
+ */
+typedef struct
+{
+	char cpf[14];
+	char name[SIZE];
+	char address[SIZE];
+
+	float wage[MONTHS];
+    float sum;
+	int code;
+
+	BirthDate bd;
+}
+Employee;
+
+/**
+ *
+ */
+Employee createEmployee(void);
 
 #endif
