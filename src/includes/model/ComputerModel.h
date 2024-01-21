841cf4984aba7af9486b0b2926228208e7cad758
Cassiano Castro <development.castro@gmail.com>
2024-01-21 00:13:38 -0300

    Added type definitions

--- src/includes/model/ComputerModel.h
+++ src/includes/model/ComputerModel.h
@@ -2,6 +2,23 @@
 
 #define COMPUTER_MODEL_H
 
+#define TAM_STRING 30
 
+typedef unsigned char byte;
+
+/**
+ *
+ */
+typedef struct
+{
+    char storageType;
+    char model[TAM_STRING];
+    char mark[TAM_STRING];
+    char processor[TAM_STRING];
+
+    int memory;
+    int storage;
+}
+Computer;
 
 #endif
