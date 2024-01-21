aeedf5a8e6e291fac432d51d2a54c07435f7fb97
Cassiano Castro <development.castro@gmail.com>
2024-01-21 00:14:59 -0300

    Added function prototypes

--- src/includes/view/ComputerView.h
+++ src/includes/view/ComputerView.h
@@ -2,6 +2,31 @@
 
 #define COMPUTER_VIEW_H
 
+#include "../model/ComputerModel.h"
 
+/**
+ *
+ */
+Computer showComputerForm(byte);
+
+/**
+ *
+ */
+void printComputerInfo(const byte, const Computer* const);
+
+/**
+ *
+ */
+const byte getComputerID(void);
+
+/**
+ *
+ */
+const unsigned int getMainMenuChoice(void);
+
+/**
+ *
+ */
+const unsigned int getUpdateMenuChoice(void);
 
 #endif
