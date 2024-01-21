8508ff2189cc62278cdae264357ce5e04964c249
Cassiano Castro <development.castro@gmail.com>
2024-01-21 01:08:05 -0300

    Added function definition: createEmployee()

---
+++ src/app/model/EmployeeModel.c
@@ -0,0 +1,18 @@
+#include "../../includes/model/EmployeeModel.h"
+
+Employee createEmployee(void)
+{
+    Employee employee = {
+        .name     = "",
+        .address  = "",
+        .cpf      = "",
+        .bd.day   = 0,
+        .bd.month = 0,
+        .bd.year  = 0,
+        .code     = 0,
+        .sum      = 0.0f,
+        .wage     = { 0.0f }
+    };
+
+    return employee;
+}
