5f2dbba5d0409bfcb924051fd9ae793e2862e003
Cassiano Castro <development.castro@gmail.com>
2024-01-22 00:09:07 -0300

    Macro and data type definitions

--- src/includes/model/EvaluationModel.h
+++ src/includes/model/EvaluationModel.h
@@ -2,6 +2,27 @@
 
 #define EVALUATION_MODEL_H
 
+#define TAM 50
+#define TAM_PROCESS 20
+#define TAM_COR 20
 
+/**
+ *
+ */
+typedef struct
+{
+	int ID;
+    int memory;
+    int storage;
+
+    float screen;
+
+    char type;
+	char mark[TAM];
+	char model[TAM];
+	char processor[TAM_PROCESS];
+	char color[TAM_COR];
+}
+Computer;
 
 #endif
