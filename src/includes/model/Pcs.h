72540d1b50d528f87b96081e7d7512f6b73bf2eb
Cassiano Castro <development.castro@gmail.com>
2024-01-22 15:12:12 -0300

    Added macro and data type declarations

--- src/includes/model/Pcs.h
+++ src/includes/model/Pcs.h
@@ -2,6 +2,23 @@
 
 #define PCS_MODEL_H
 
+#define MAX_SIZE_STR 30
 
+typedef unsigned char byte;
+
+/**
+ *
+ */
+typedef struct
+{
+    char storageType;
+	char model[MAX_SIZE_STR];
+	char mark[MAX_SIZE_STR];
+	char processor[MAX_SIZE_STR];
+
+    int memory;
+    int storage;
+}
+Computer;
 
 #endif
