01dc47e527c0a6f7ccad3d39586c3d7f52d09cc9
Cassiano Castro <development.castro@gmail.com>
2024-01-22 14:31:56 -0300

    Added macro and type definitions

--- src/includes/model/Book.h
+++ src/includes/model/Book.h
@@ -2,6 +2,27 @@
 
 #define BOOK_MODEL_H
 
+#define TAMANHO 50
 
+typedef unsigned char byte;
+
+/**
+ *
+ */
+typedef struct
+{
+	char title[TAMANHO];
+	char author[TAMANHO];
+	char publishing[TAMANHO];
+	char subject[TAMANHO];
+
+	unsigned short release;
+}
+Book;
+
+/**
+ *
+ */
+Book createBook(void);
 
 #endif
