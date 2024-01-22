9d556ec6db7ef9fa65c1ce6368585281de990636
Cassiano Castro <development.castro@gmail.com>
2024-01-22 01:04:05 -0300

    Added type definitions and function prototype

--- src/includes/model/CarModel.h
+++ src/includes/model/CarModel.h
@@ -2,6 +2,27 @@
 
 #define CARS_MODEL_H
 
+#define STRING_SIZE 20
 
+typedef unsigned char byte;
+
+/**
+ *
+ */
+typedef struct
+{
+	char model[STRING_SIZE];
+	char mark[STRING_SIZE];
+	char placa[STRING_SIZE];
+	char color[STRING_SIZE];
+
+	unsigned short release;
+}
+Car;
+
+/**
+ *
+ */
+Car createCar(void);
 
 #endif
