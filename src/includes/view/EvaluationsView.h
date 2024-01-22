f313e2b97e31a62436e5a0ad43fab40bd657cfbe
Cassiano Castro <development.castro@gmail.com>
2024-01-22 00:04:15 -0300

    Added function prototypes

--- src/includes/view/EvaluationsView.h
+++ src/includes/view/EvaluationsView.h
@@ -2,6 +2,29 @@
 
 #define EVALUATION_VIEW_H
 
+/**
+ *
+ */
+void showComputerForm(unsigned short int);
 
+/**
+ *
+ */
+void printComputer(const Computer* const);
+
+/**
+ *
+ */
+unsigned int getComputerID(void);
+
+/**
+ *
+ */
+const unsigned int getMainMenuChoice(void);
+
+/**
+ *
+ */
+const unsigned int getUpdateMenuChoice(void);
 
 #endif
