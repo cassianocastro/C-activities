68cacc709dc720ad3f2ba276cec241085d8ed1b5
Cassiano Castro <development.castro@gmail.com>
2024-01-24 15:27:57 -0300

    Added function definition

--- src/app/model/Agenda.c
+++ src/app/model/Agenda.c
@@ -5,3 +5,16 @@ const unsigned short int CURRENT_YEAR = 2024;
 Contact* contacts;
 
 unsigned short int num;
+
+bool searchID(const byte id)
+{
+    for ( register byte i = 0, j = 0; i < num; ++i )
+    {
+		if ( i == id )
+        {
+            return true;
+		}
+	}
+
+    return false;
+}
